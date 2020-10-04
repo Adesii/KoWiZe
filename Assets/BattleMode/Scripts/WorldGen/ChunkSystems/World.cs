160a2c6e45a8d56fe8235194ffeda930fa86889c
Adesi <kopietz123@gmail.com>
2020-10-04 21:21:51 +0200

    Reorganized Folders

---
+++ Assets/BattleMode/Scripts/WorldGen/ChunkSystems/World.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    private static World _main;
+    public static World main
+    {
+        get { return _main; }
+    }
+
+    public static int chunkSize = 32;
+    public static GameObject world;
+    public GameObject Player;
+
+    public int viewDistance = 5;
+    public Material terrianMaterial;
+
+    private Dictionary<ChunkPoint, Chunk> _chunks = new Dictionary<ChunkPoint, Chunk>();
+    private Dictionary<ChunkPoint, GameObject> _goCache = new Dictionary<ChunkPoint, GameObject>();
+
+
+    public void Awake()
+    {
+        if (_main == null)
+            _main = this;
+    }
+    public enum LODLEVELS
+    {
+        LOD0,
+        LOD1,
+        LOD2,
+        LOD3
+    }
+    [Serializable]
+    public struct ChunkPoint
+    {
+        private readonly int _x;
+        private readonly int _z;
+
+        public ChunkPoint(int x, int z)
+        {
+            this._x = x;
+            this._z = z;
+        }
+
+        public int X
+        {
+            get { return _x; }
+        }
+
+        public int Z
+        {
+            get { return _z; }
+        }
+
+        public ChunkPoint Move(Vector2 offset)
+        {
+            return new ChunkPoint(_x + (int)offset.x, _z + (int)offset.y);
+        }
+
+        public static Vector2 operator -(ChunkPoint a, ChunkPoint b)
+        {
+            return new Vector2(a._x - b._x, a._z - b._z);
+        }
+
+        public static Vector2 operator +(ChunkPoint a, ChunkPoint b)
+        {
+            return new Vector2(a._x + b._x, a._z + b._z);
+        }
+
+        public static bool operator ==(ChunkPoint a, ChunkPoint b)
+        {
+            return a._x == b._x && a._z == b._z;
+        }
+
+        public static bool operator !=(ChunkPoint a, ChunkPoint b)
+        {
+            return a._x != b._x || a._z != b._z;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ChunkPoint)) return false;
+            var c = (ChunkPoint)obj;
+
+            return c.X == X && c.Z == Z;
+
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 23;
+            hash = hash * 31 + _x;
+            hash = hash * 31 + _z;
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            return "(" + _x + ", " + _z + ")";
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+}
---
+++ Assets/BattleMode/Scripts/WorldGen/ChunkSystems/classes/Chunk.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk
+{
+    public List<Vector3> Verticies = new List<Vector3>();
+    public List<int> Indices = new List<int>();
+    public World.ChunkPoint Position;
+    public float[,] noisemap;
+
+
+    public WorldTypes worldType;
+
+    public Chunk(World.ChunkPoint position,float[,] noisemap)
+    {
+        this.Position = position;
+        this.noisemap = noisemap;
+    }
+    public int PosToIndex(int x, int y, int z)
+    {
+
+        var index = x * (World.chunkSize + 1) + y * (World.chunkSize + 1) + z;
+        return index;
+        //return x + ChunkSize * (y + ChunkSize * z);
+    }
+    public GameObject GenerateObject(Material material, Vector3 origin)
+    {
+        if (Verticies.Count == 0)
+            return null;
+
+        var mesh = new Mesh();
+        mesh.vertices = Verticies.ToArray();
+        mesh.triangles = Indices.ToArray();
+        mesh.uv = new Vector2[mesh.vertices.Length];
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        var go = new GameObject("TerrainChunk");
+        var mf = go.AddComponent<MeshFilter>();
+        var mr = go.AddComponent<MeshRenderer>();
+        var collider = go.AddComponent<MeshCollider>();
+        mf.sharedMesh = mesh;
+        mr.sharedMaterial = material;
+        collider.sharedMesh = mesh;
+        go.transform.position = origin;
+        //go.transform.Rotate(0f, 0f, -180f);
+
+        return go;
+    }
+    public void Dispose()
+    {
+        Verticies.Clear();
+        Indices.Clear();
+        noisemap = null;
+    }
+}
