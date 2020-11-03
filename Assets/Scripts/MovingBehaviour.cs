982075e5edcd263bbf080cc55daf37ab95112f2e
Samueltz <48752580+SamueltZeidler@users.noreply.github.com>
2020-11-03 10:40:39 +0100

    initial creation of movingbehaviours

---
+++ Assets/Scripts/MovingBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Defines an agent that moves
+    public class seek
+    {
+        public Vector2 position;
+        public Quaternion rotation;
+        public Vector3 velocity;
+        public float maxForce = 5; // rate of acceleration
+        public float maxSpeed = 4; //grid squares the second
+        public seek(Vector2 pos)
+        {
+            this.position = pos;
+            this.velocity = Vector2.zero;
+            this.maxForce = 5;
+            this.maxSpeed = 4;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
