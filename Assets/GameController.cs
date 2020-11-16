4102c10d935c403d19a294ff6ff9f8b173e8ae85
Adesi <kopietz123@gmail.com>
2020-11-16 14:36:00 +0100

    started work on citySystem

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 
@@ -10,24 +11,19 @@ public class GameController : MonoBehaviour
     public static GameController Instance { get { return _instance; } }
 
     [Header("Tree Generation Variables")]
-    public GameObject treePrefab;
-    public List<GameObject> placeableModels;
-    public int maxPlacedTrees = 200;
-    public int maxForestSize = 10;
-    public int minForestSize = 2;
-    public float heightLimit = 14f;
-    public float minHeight = 1.4f;
-    [Range(0, 1)]
-    public float successProcent = 0.50f;
+    public TreeSetting treeSettings = new TreeSetting();
+    [Header("CitySettings")]
+    public CitySetting citySettings = new CitySetting();
 
-    public static GameObject TreePrefab { get => _instance.treePrefab; set => _instance.treePrefab = value; }
-    public static List<GameObject> PlaceableModels { get => _instance.placeableModels; set => _instance.placeableModels = value; }
-    public static int MaxPlacedTrees { get => _instance.maxPlacedTrees; set => _instance.maxPlacedTrees = value; }
-    public static int MaxForestSize { get => _instance.maxForestSize; set => _instance.maxForestSize = value; }
-    public static int MinForestSize { get => _instance.minForestSize; set => _instance.minForestSize = value; }
-    public static float SuccessProcent { get => _instance.successProcent; set => _instance.successProcent = value; }
-    public static float HeightLimit { get => _instance.heightLimit; set => _instance.heightLimit = value; }
-    public static float MinHeight { get => _instance.minHeight; set => _instance.minHeight = value; }
+    public static GameObject TreePrefab { get => _instance.treeSettings.treePrefab; set => _instance.treeSettings.treePrefab = value; }
+    public static List<GameObject> PlaceableModels { get => _instance.treeSettings.placeableModels; set => _instance.treeSettings.placeableModels = value; }
+    public static int MaxPlacedTrees { get => _instance.treeSettings.maxPlacedTrees; set => _instance.treeSettings.maxPlacedTrees = value; }
+    public static int MaxForestSize { get => _instance.treeSettings.maxForestSize; set => _instance.treeSettings.maxForestSize = value; }
+    public static int MinForestSize { get => _instance.treeSettings.minForestSize; set => _instance.treeSettings.minForestSize = value; }
+    public static float SuccessProcent { get => _instance.treeSettings.successProcent; set => _instance.treeSettings.successProcent = value; }
+    public static float HeightLimit { get => _instance.treeSettings.heightLimit; set => _instance.treeSettings.heightLimit = value; }
+    public static float MinHeight { get => _instance.treeSettings.minHeight; set => _instance.treeSettings.minHeight = value; }
+    public static CitySetting CitySettings { get => _instance.citySettings; set => _instance.citySettings = value; }
 
 
 
@@ -44,5 +40,39 @@ public class GameController : MonoBehaviour
             DontDestroyOnLoad(_instance.gameObject);
         }
     }
+    [Serializable]
+    public class TreeSetting
+    {
+        public GameObject treePrefab;
+        public List<GameObject> placeableModels;
+        public int maxPlacedTrees = 200;
+        public int maxForestSize = 10;
+        public int minForestSize = 2;
+        public float heightLimit = 14f;
+        public float minHeight = 1.4f;
+        [Range(0, 1)]
+        public float successProcent = 0.50f;
+    }
+    [Serializable]
+    public class CitySetting
+    {
 
+        public List<perPlayerCitySettings> perPlayerSettings = new List<perPlayerCitySettings>();
+
+        [Serializable]
+        public class perPlayerCitySettings
+        {
+            public int playerID;
+            public float exponentialFoodProduction = 2f;
+            public float exponentialScienceProduction = 2f;
+
+            public float science;
+            public float gold;
+
+            public List<citySystem> playerCities;
+
+
+        }
+    }
 }
+
---
+++ Assets/citySystem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class citySystem : MonoBehaviour
+{
+
+    public float wood;
+    public float stone;
+    public float iron;
+}
