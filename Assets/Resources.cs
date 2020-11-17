0bd48ed0cf9d34751c5f1a638e318fc26ccf0ae3
Adesi <kopietz123@gmail.com>
2020-11-17 09:56:16 +0100

    Added ResourceClass to streamline Addition and Removing Resources From a City

---
+++ Assets/Resources.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+ [Serializable]
+public class Resources
+{
+    public enum ResourceTypes{
+        Wood,
+        Stone,
+        Iron,
+        Science,
+        Food
+    }
+
+    [Serializable]
+    public class ResourceClass
+    {
+        public ResourceTypes ResourceType;
+        public float currentAmount;
+        public float maxCapacity;
+
+        public bool AddResource(float amount)
+        {
+            if (currentAmount + amount <= maxCapacity)
+                currentAmount += amount;
+            else
+                return false;
+            return true;
+        }
+        public bool RemoveResource(float amount)
+        {
+            if (currentAmount - amount > 0)
+                currentAmount -= amount;
+            else
+                return false;
+            return true;
+        }
+
+        public void AddToLimit(float amount)
+        {
+            maxCapacity += amount;
+        }
+        public void RemoveFromLimit(float amount)
+        {
+            if (maxCapacity >= 0f) maxCapacity -= amount;
+        }
+
+        public void SetLimit(float amount)
+        {
+            maxCapacity = amount;
+        }
+        public void SetAmount(float amount)
+        {
+            currentAmount = amount;
+        }
+
+        public ResourceClass(ResourceTypes type,float maxAmount,float startingAmount)
+        {
+            ResourceType = type;
+            currentAmount = startingAmount;
+            maxCapacity = maxAmount;
+        }
+
+        public ResourceClass(ResourceTypes type)
+        {
+            ResourceType = type;
+            currentAmount = 0;
+            maxCapacity = 0;
+        }
+    }
+}
--- Assets/citySystem.cs
+++ Assets/citySystem.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Resources;
 
 public class citySystem : MonoBehaviour
 {
+    public int cityID;
 
-    public float wood;
-    public float stone;
-    public float iron;
+    public List<ResourceClass> resources = new List<ResourceClass>();
+
+    public citySystem(List<ResourceClass> resources)
+    {
+        this.resources = resources;
+    }
+    public citySystem()
+    {
+        resources.Add(new ResourceClass(ResourceTypes.Wood, 100, 10));
+        resources.Add(new ResourceClass(ResourceTypes.Stone, 100, 10));
+        resources.Add(new ResourceClass(ResourceTypes.Food, 100, 10));
+        resources.Add(new ResourceClass(ResourceTypes.Iron, 100, 10));
+    }
+
+    public ResourceClass GetResource(ResourceTypes resourceType)
+    {
+        for (int i = 0; i < resources.Count; i++)
+        {
+            if(resources[i].ResourceType == resourceType)
+            {
+                return resources[i];
+            }
+        }
+        return null;
+    }
 }
