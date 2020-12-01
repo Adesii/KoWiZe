9cd48d5533a4d9b8fc6ce7fde8c9b0c94c20c9db
Samueltz <48752580+SamueltZeidler@users.noreply.github.com>
2020-12-01 10:53:53 +0100

    started localization system

---
+++ Assets/Scripts/Localization/TextLocalisationUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextLocalisationUI : MonoBehaviour
+{
+    TextMeshProUGUI textField;
+    public string key;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField = GetComponent<TextMeshProUGUI>();
+        textField.text = localization.GetLocalisedValue(key);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Localization/localization.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class localization : MonoBehaviour
+{
+    public enum Language
+    {
+        English
+    }
+    public static Language language = Language.English;
+
+    private static Dictionary<string, string> localisedEN;
+    public static bool isInit;
+
+    public static void Init()
+    {
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.LoadCSV();
+
+        localisedEN = csvLoader.GetDictionaryValues("en");
+
+        isInit = true;
+    }
+    public static string GetLocalisedValue(string key)
+    {
+        if (!isInit) { Init(); }
+        string value = key;
+        switch (language)
+        {
+            case Language.English:
+                localisedEN.TryGetValue(key, out value);
+                break;
+        }
+        return value;
+    }
+}
