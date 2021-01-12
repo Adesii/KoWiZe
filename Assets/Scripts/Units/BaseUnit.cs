2c91900495c0c814f86250d9d595c85b2b553a41
Samueltz <48752580+SamueltZeidler@users.noreply.github.com>
2021-01-12 10:53:59 +0100

    Created BaseUnit

---
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum unit_type
+{
+    Melee,
+    Ranged,
+    Siege
+}
+enum unit_subtype
+{
+    None,
+    Cavalry
+}
+
+enum unit_building
+{
+    Barracks,
+    Archery_Range,
+    Stable,
+    Workshop
+}
+
+public interface IUnit
+{
+
+}
+public abstract class BaseUnit : ScriptableObject, IUnit
+{
+    public string name = "base";
+    public unit_type type;
+    public unit_subtype subtype;
+    public float hp = 100f;
+    public float armor = 1f;
+    public float attack = 10f;
+    public float speed = 100f;
+    public Dictionary<ResourceClass.ResourceTypes, float> costs;
+    public unit_building building;
+    public float build_time = 10f;
+    //todo technology unlock
+    public string description = "Lorem Ipsum Dolor Sit Amet";
+    public Dictionary<IUnit, float> unit_strenght;
+}
