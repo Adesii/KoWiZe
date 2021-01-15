f16a94d2336424dd0fa125df14ee1f98f7296272
Samueltz <48752580+SamueltZeidler@users.noreply.github.com>
2021-01-15 08:25:55 +0100

    Added Melee,Ranged and SiegeUnit Base Class

--- Assets/Scripts/Units/BaseUnit.cs
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum unit_type
 {
@@ -27,6 +28,7 @@ public interface IUnit
 }
 public abstract class BaseUnit : ScriptableObject, IUnit
 {
+    public Image UnitIcon;
     public string Unit_name = "base";
     public unit_type type;
     public unit_subtype subtype;
@@ -38,6 +40,41 @@ public abstract class BaseUnit : ScriptableObject, IUnit
     public unit_building building;
     public float build_time = 10f;
     //todo technology unlock
+    [Multiline]
     public string description = "Lorem Ipsum Dolor Sit Amet";
+
     public Dictionary<IUnit, float> unit_strenght;
+
+
+    public void TakeDamage(float amount)
+    {
+
+    }
+    public void MoveUnit()
+    {
+        
+    }
+    public void Attack(float amount)
+    {
+
+    }
+}
+[CreateAssetMenu(fileName = "MeeleUnit", menuName = "KoWiZe Custom Assets/Units/Melee")]
+public class MeleeUnit : BaseUnit
+{
+
+
+}
+[CreateAssetMenu(fileName ="RangedUnit", menuName ="KoWiZe Custom Assets/Units/Ranged" )]
+public class RangedUnit : BaseUnit
+{
+    public float UnitRange = 100f;
+
+}
+
+[CreateAssetMenu(fileName="SiegeUnit",menuName="KoWiZe Custom Assets/Units/Siege")]
+public class SiegeUnit : BaseUnit
+{
+
+
 }
