64b9a0d05412b27901d6eaa06e0b33cff0617ab0
Adesi <kopietz123@gmail.com>
2021-02-08 00:12:01 +0100

    Started Work on MainMenuLobby

--- Assets/Scripts/networking/AORNetworkRoomPlayer.cs
+++ Assets/Scripts/networking/AORNetworkRoomPlayer.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
+using TMPro;
+using Steamworks;
 
 /*
 	Documentation: https://mirror-networking.com/docs/Components/NetworkRoomPlayer.html
@@ -14,6 +17,76 @@ using Mirror;
 /// </summary>
 public class AORNetworkRoomPlayer : NetworkRoomPlayer
 {
+    [SyncVar(hook = nameof(HandleSteamIdUpdated))]
+    private ulong steamId;
+
+    [SerializeField] private RawImage profileImage = null;
+    [SerializeField] private TMP_Text displayNameText = null;
+
+    protected Callback<AvatarImageLoaded_t> avatarImageLoaded;
+
+    #region Server
+
+    public void SetSteamId(ulong steamId)
+    {
+        this.steamId = steamId;
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        avatarImageLoaded = Callback<AvatarImageLoaded_t>.Create(OnAvatarImageLoaded);
+    }
+
+    private void HandleSteamIdUpdated(ulong oldSteamId, ulong newSteamId)
+    {
+        var cSteamId = new CSteamID(newSteamId);
+
+        displayNameText.text = SteamFriends.GetFriendPersonaName(cSteamId);
+
+        int imageId = SteamFriends.GetLargeFriendAvatar(cSteamId);
+
+        if (imageId == -1) { return; }
+
+        profileImage.texture = GetSteamImageAsTexture(imageId);
+    }
+
+    private void OnAvatarImageLoaded(AvatarImageLoaded_t callback)
+    {
+        if (callback.m_steamID.m_SteamID != steamId) { return; }
+
+        profileImage.texture = GetSteamImageAsTexture(callback.m_iImage);
+    }
+
+    private Texture2D GetSteamImageAsTexture(int iImage)
+    {
+        Texture2D texture = null;
+
+        bool isValid = SteamUtils.GetImageSize(iImage, out uint width, out uint height);
+
+        if (isValid)
+        {
+            byte[] image = new byte[width * height * 4];
+
+            isValid = SteamUtils.GetImageRGBA(iImage, image, (int)(width * height * 4));
+
+            if (isValid)
+            {
+                texture = new Texture2D((int)width, (int)height, TextureFormat.RGBA32, false, true);
+                texture.LoadRawTextureData(image);
+                texture.Apply();
+            }
+        }
+
+        return texture;
+    }
+
+    #endregion
+
+
     #region Start & Stop Callbacks
 
     /// <summary>
@@ -33,7 +106,6 @@ public class AORNetworkRoomPlayer : NetworkRoomPlayer
     /// Called on every NetworkBehaviour when it is activated on a client.
     /// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
     /// </summary>
-    public override void OnStartClient() { }
 
     /// <summary>
     /// This is invoked on clients when the server has caused this object to be destroyed.
@@ -103,4 +175,7 @@ public class AORNetworkRoomPlayer : NetworkRoomPlayer
     }
 
     #endregion
+
+
+
 }
